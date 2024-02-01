30ba370a93f5ac1d77c636d98d7f9b061bade503
slapiashvili <sslapiashvili@gmail.com>
2024-02-01 20:13:22 +0400

    add marks  to WelcomeView, refine code

--- Spontaneous/Scenes/Onboarding/WelcomeView.swift
+++ Spontaneous/Scenes/Onboarding/WelcomeView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct WelcomeView: View {
     
+    // MARK: - Properties
+    
     let images: [Image] = (1...10).map { String(format: "girl%01d", $0) }.map { Image($0) }
     @ObservedObject private var counter = Counter(interval: 0.15)
 
+    // MARK: - Body
+    
     var body: some View {
+        welcomeContent
+    }
+
+    // MARK: - Private Properties
+
+    private var welcomeContent: some View {
         VStack {
             Spacer()
-            images[counter.value % images.count]
+            girlImage
                 .resizable()
                 .scaledToFill()
                 .frame(width: UIScreen.main.bounds.width * 2/5, height: UIScreen.main.bounds.height * 2/5)
@@ -24,16 +34,26 @@ struct WelcomeView: View {
                     animateWithSpring()
                 }
 
-            Text("We make decision-making easier")
-                .font(Font.custom("Jura", size: 20, relativeTo: .title2))
-                .foregroundColor(.neoTextOpposite)
-                .multilineTextAlignment(.center)
-                .padding()
+            welcomeText
 
             Spacer()
         }
     }
+    
+    private var girlImage: Image {
+        images[counter.value % images.count]
+    }
 
+    private var welcomeText: some View {
+        Text("We make decision-making easier")
+            .font(Font.custom("Jura", size: 20, relativeTo: .title2))
+            .foregroundColor(.neoTextOpposite)
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+
+    // MARK: - Private Methods
+    
     private func animateWithSpring() {
         withAnimation(.interpolatingSpring(stiffness: 10, damping: 10)) {
             counter.value += 1
@@ -41,6 +61,7 @@ struct WelcomeView: View {
     }
 }
 
+// MARK: - Counter for the animation
 
 private class Counter: ObservableObject {
     private var timer: Timer?
